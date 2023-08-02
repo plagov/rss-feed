8a72dd7f63743f79b69f0e1a85e91e4be7cc5809
Vitali Plagov <vplagov@pm.me>
2023-08-03 00:18:36 +0300

    Add controller to update blog
    Also, update tests and minor refactoring.

--- src/main/java/io/plagov/rssfeed/controller/BlogController.java
+++ src/main/java/io/plagov/rssfeed/controller/BlogController.java
@@ -3,9 +3,17 @@ package io.plagov.rssfeed.controller;
 import io.plagov.rssfeed.dao.BlogDao;
 import io.plagov.rssfeed.domain.Blog;
 import io.plagov.rssfeed.domain.request.NewBlog;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -15,18 +23,32 @@ public class BlogController {
 
     private final BlogDao blogDao;
 
+    private final Logger logger = LoggerFactory.getLogger(BlogController.class);
+
     public BlogController(BlogDao blogDao) {
         this.blogDao = blogDao;
     }
 
     @PostMapping("/add")
-    public ResponseEntity<String> addNewBlog(@RequestBody NewBlog blog) {
-        blogDao.addNewBlog(blog.name(), blog.feedUrl());
-        return ResponseEntity.status(HttpStatus.CREATED).body("Blog %s added successfully".formatted(blog.name()));
+    public ResponseEntity<Integer> addNewBlog(@RequestBody NewBlog blog) {
+        var blogId = blogDao.addNewBlog(blog.name(), blog.feedUrl());
+        return new ResponseEntity<>(blogId, HttpStatus.CREATED);
     }
 
     @GetMapping("/all")
     public List<Blog> getAllBlogs() {
         return blogDao.getAllBlogs();
+    }
+
+    @PutMapping("/{id}")
+    public Blog updateBlog(@PathVariable int id, @RequestBody NewBlog blog) {
+        logger.info("Update blog with id {}.", id);
+        blogDao.updateBlogById(id, blog.name(), blog.feedUrl());
+        return blogDao.getBlogById(id);
+    }
+
+    @GetMapping("/{id}")
+    public Blog getBlogById(@PathVariable int id) {
+        return blogDao.getBlogById(id);
     }
 }
--- src/main/java/io/plagov/rssfeed/dao/BlogDao.java
+++ src/main/java/io/plagov/rssfeed/dao/BlogDao.java
@@ -1,32 +1,52 @@
 package io.plagov.rssfeed.dao;
 
 import io.plagov.rssfeed.domain.Blog;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class BlogDao {
 
     private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
 
     public BlogDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("blogs")
+                .usingGeneratedKeyColumns("id");
     }
 
     public List<Blog> getAllBlogs() {
-        return jdbcTemplate.query("SELECT * FROM blogs", (rs, i) -> new Blog(
+        return jdbcTemplate.query("SELECT * FROM blogs", mapBlogRow());
+    }
+
+    @NotNull
+    private static RowMapper<Blog> mapBlogRow() {
+        return (rs, i) -> new Blog(
                 rs.getInt("id"),
                 rs.getString("name"),
                 rs.getString("feed_url")
-        ));
+        );
     }
 
-    public void addNewBlog(String blogName, String feedUrl) {
-        jdbcTemplate.update("INSERT INTO blogs (name, feed_url) VALUES (?, ?)",
-                blogName,
-                feedUrl
-        );
+    public int addNewBlog(String blogName, String feedUrl) {
+        return simpleJdbcInsert.executeAndReturnKey(Map.of("name", blogName, "feed_url", feedUrl)).intValue();
+    }
+
+    public void updateBlogById(int blogId, String blogName, String feedUrl) {
+        var sql = "UPDATE blogs SET name = ?, feed_url = ? WHERE id = ?";
+        jdbcTemplate.update(sql, blogName, feedUrl, blogId);
+    }
+
+    public Blog getBlogById(int blogId) {
+        var sql = "SELECT * FROM blogs WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, mapBlogRow(), blogId);
     }
 }
