424cbe81043b1183f5841e995157c70da917466e
Vitali Plagov <vplagov@pm.me>
2023-11-25 19:53:16 +0200

    Define a custom task to install Playwright on CI
    This task is needed to install the Playwright and its dependencies to be
    able to run tests on GitHub Actions CI.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,9 @@ import org.flywaydb.gradle.task.FlywayMigrateTask
 
 plugins {
     java
-    application
     id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.3"
     id("org.flywaydb.flyway") version "9.22.3"
-}
-
-application {
-    mainClass.set("com.microsoft.playwright.CLI")
 }
 
 val testContainersVersion = "1.19.1"
@@ -60,4 +55,10 @@ java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
     }
+}
+
+tasks.register<JavaExec>("playwrightInstall") {
+    classpath(sourceSets["test"].runtimeClasspath)
+    mainClass.set("com.microsoft.playwright.CLI")
+    args = listOf("install-deps")
 }
