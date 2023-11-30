2407931f030b04ae1e6985f2ce1dfbb4f3fe4995
Vitali Plagov <vplagov@pm.me>
2023-11-30 21:47:52 +0200

    Remove URL column & make post title clickable

--- src/test/java/io/plagov/rssfeed/view/PostViewTest.java
+++ src/test/java/io/plagov/rssfeed/view/PostViewTest.java
@@ -1,5 +1,6 @@
 package io.plagov.rssfeed.view;
 
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
 import io.plagov.rssfeed.E2eBaseTest;
 import io.plagov.rssfeed.configuration.ContainersConfig;
 import org.assertj.core.api.Assertions;
@@ -37,5 +38,21 @@ class PostViewTest extends E2eBaseTest {
         page.navigate("http://localhost:" + port);
         var posts = page.querySelectorAll("table tbody tr");
         Assertions.assertThat(posts).hasSize(2);
+    }
+
+    @Test
+    @Sql("/sql/posts/add_posts.sql")
+    void canViewCorrectNumberOfColumns() {
+        page.navigate("http://localhost:" + port);
+        var columnHeadings = page.locator("table thead tr th").allTextContents();
+        Assertions.assertThat(columnHeadings).containsExactly("ID", "Title", "Date added");
+    }
+
+    @Test
+    @Sql("/sql/posts/add_posts.sql")
+    void titleColumnContainsLinks() {
+        page.navigate("http://localhost:" + port);
+        var postTitleCell = page.locator("tbody > tr:nth-child(1) > td:nth-child(2) > a");
+        PlaywrightAssertions.assertThat(postTitleCell).hasAttribute("href", "post1.com");
     }
 }
