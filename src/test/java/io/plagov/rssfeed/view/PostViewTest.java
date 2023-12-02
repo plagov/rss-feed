b20c897c219a64043a8094310575765b19fada82
Vitali Plagov <vplagov@pm.me>
2023-12-02 20:21:01 +0200

    Remove URL column & make Title column clickable (#26)
    * Remove URL column & make post title clickable
    
    * Center the table and make column width dynamic
    
    * Use Flyway extensions for e2e tests
    
    When the `@FlywayTest` annotation is used on the class level, then it
    will clean the database and run migrations from the default location
    before each test.

--- src/test/java/io/plagov/rssfeed/view/PostViewTest.java
+++ src/test/java/io/plagov/rssfeed/view/PostViewTest.java
@@ -1,35 +1,25 @@
 package io.plagov.rssfeed.view;
 
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
 import io.plagov.rssfeed.E2eBaseTest;
 import io.plagov.rssfeed.configuration.ContainersConfig;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
+import org.flywaydb.test.annotation.FlywayTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @Import(ContainersConfig.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@FlywayTest
 class PostViewTest extends E2eBaseTest {
 
     @LocalServerPort
     private int port;
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    @SuppressWarnings("'Delete' statement without 'where' clears all data in the table")
-    @AfterEach
-    void cleanUp() {
-        jdbcTemplate.execute("DELETE FROM posts");
-        jdbcTemplate.execute("DELETE FROM blogs");
-    }
 
     @Test
     @Sql("/sql/posts/add_posts.sql")
@@ -37,5 +27,21 @@ class PostViewTest extends E2eBaseTest {
         page.navigate("http://localhost:" + port);
         var posts = page.querySelectorAll("table tbody tr");
         Assertions.assertThat(posts).hasSize(2);
+    }
+
+    @Test
+    @Sql("/sql/posts/add_posts.sql")
+    void canViewCorrectNumberOfColumns() {
+        page.navigate("http://localhost:" + port);
+        var columnHeadings = page.locator("table thead tr th").allTextContents();
+        Assertions.assertThat(columnHeadings).containsExactly("ID", "Title", "Date added");
+    }
+
+    @Test
+    @Sql("/sql/posts/add_posts.sql")
+    void titleColumnContainsLinks() {
+        page.navigate("http://localhost:" + port);
+        var postTitleCell = page.locator("tbody > tr:nth-child(1) > td:nth-child(2) > a");
+        PlaywrightAssertions.assertThat(postTitleCell).hasAttribute("href", "https://post1.com");
     }
 }
--- src/test/resources/flyway/migrations_local/V202309012332__add_posts.sql
+++ src/test/resources/flyway/migrations_local/V202309012332__add_posts.sql
@@ -1,3 +1,3 @@
-INSERT INTO posts (blog_id, post_name, post_url, is_read, date_added) VALUES (1, 'Post 1', 'post1.com', false, '2021-01-01');
-INSERT INTO posts (blog_id, post_name, post_url, is_read, date_added) VALUES (1, 'Post 2', 'post2.com', false, '2021-01-01');
-INSERT INTO posts (blog_id, post_name, post_url, is_read, date_added) VALUES (1, 'Post 3', 'post3.com', true, '2021-01-01');
+INSERT INTO posts (blog_id, post_name, post_url, is_read, date_added) VALUES (1, 'Post 1', 'https://post1.com', false, '2021-01-01');
+INSERT INTO posts (blog_id, post_name, post_url, is_read, date_added) VALUES (1, 'Post 2', 'https://post2.com', false, '2021-01-01');
+INSERT INTO posts (blog_id, post_name, post_url, is_read, date_added) VALUES (1, 'Post 3', 'https://post3.com', true, '2021-01-01');
