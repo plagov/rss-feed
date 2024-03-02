1c7be3ad7921d512d98f6bedbe3566bea19836e2
Vitali Plagov <vplagov@pm.me>
2024-03-02 12:52:07 +0200

    Dependency upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,13 @@ import org.flywaydb.gradle.task.FlywayMigrateTask
 
 plugins {
     java
-    id("org.springframework.boot") version "3.2.2"
+    id("org.springframework.boot") version "3.2.3"
     id("io.spring.dependency-management") version "1.1.4"
     id("org.flywaydb.flyway") version "10.8.1"
+    id("com.github.ben-manes.versions") version "0.51.0"
 }
 
-val testContainersVersion = "1.19.5"
+val testContainersVersion = "1.19.6"
 
 group = "io.plagov"
 version = "0.0.1-SNAPSHOT"
