d1e1e28743b27115400b85c64e6b7023f19010dc
Vitali Plagov <vplagov@pm.me>
2024-11-12 22:45:22 +0200

    Define Testcontainers image in properties file
    When a docker tag used for Testcontainers is defined in an external
    properties file, then it is possible to write a regex manager for the
    Renovate so that it will scan this file, extract an image name and tag
    and will scan it against the docker. This way, the image will eb always
    up to date.

--- src/test/java/io/plagov/rssfeed/configuration/ContainersConfig.java
+++ src/test/java/io/plagov/rssfeed/configuration/ContainersConfig.java
@@ -1,17 +1,23 @@
 package io.plagov.rssfeed.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
+@PropertySource("classpath:testcontainers-images.properties")
 public class ContainersConfig {
+
+    @Value("${database}")
+    private String databaseImage;
 
     @Bean
     @ServiceConnection
     public PostgreSQLContainer<?> postgreSQLContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:15.6"));
+        return new PostgreSQLContainer<>(DockerImageName.parse(databaseImage));
     }
 }
