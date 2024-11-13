e878cf1611cf99aeaec8387cf164382165bf9c44
Vitali Plagov <vplagov@pm.me>
2024-11-13 23:07:46 +0200

    Try to log authenticated user's email

--- src/main/java/io/plagov/rssfeed/view/PostsView.java
+++ src/main/java/io/plagov/rssfeed/view/PostsView.java
@@ -1,6 +1,10 @@
 package io.plagov.rssfeed.view;
 
 import io.plagov.rssfeed.dao.PostDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +16,18 @@ public class PostsView {
 
     private final PostDao postDao;
 
+    private final Logger logger = LoggerFactory.getLogger(PostsView.class);
+
     public PostsView(PostDao postDao) {
         this.postDao = postDao;
     }
 
     @GetMapping("/")
-    public String getMainView(Model model) {
+    public String getMainView(@AuthenticationPrincipal OAuth2User principal, Model model) {
+        if (principal != null) {
+            var email = principal.getAttributes().get("email").toString();
+            logger.info("User email is: {}", email);
+        }
         var posts = postDao.getAllUnreadPosts();
         model.addAttribute("posts", posts);
         return "index";
