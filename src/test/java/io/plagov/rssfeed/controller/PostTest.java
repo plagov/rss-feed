99c1b470cfbce3edf034d109c6cfe3b5fb8d5bbc
Vitali Plagov <vplagov@pm.me>
2025-07-27 23:45:09 +0300

    Use MockMvcTester for existing test

--- src/test/java/io/plagov/rssfeed/controller/PostTest.java
+++ src/test/java/io/plagov/rssfeed/controller/PostTest.java
@@ -5,14 +5,18 @@ import io.plagov.rssfeed.configuration.FakeClockConfiguration;
 import io.plagov.rssfeed.domain.request.PostRequest;
 import io.plagov.rssfeed.domain.response.PostResponse;
 import io.plagov.rssfeed.service.PostService;
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.Clock;
@@ -24,6 +28,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(classes = FakeClockConfiguration.class)
 @Import(ContainersConfig.class)
 @TestPropertySource(properties = "ALLOWED_USER_EMAIL = test@example.com")
+@AutoConfigureMockMvc
 class PostTest {
 
     @Autowired
@@ -38,16 +43,27 @@ class PostTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private MockMvcTester mockMvc;
+
+    private static final String TEST_TOKEN = "test-token-123";
+
     @AfterEach
     void tearDown() {
         jdbcTemplate.execute("DELETE FROM posts");
     }
 
     @Test
-    @Sql("/sql/posts/add_posts.sql")
+    @Sql({"/sql/tokens/add_test_token.sql", "/sql/posts/add_posts.sql"})
     void shouldReturnListOfUnreadPosts() {
-        var allUnreadPosts = postController.getAllUnreadPosts();
-        assertThat(allUnreadPosts).hasSize(2);
+        var httpResponse = mockMvc.get()
+            .uri("/api/posts/unread")
+            .header("X-API-Token", TEST_TOKEN)
+            .exchange();
+        assertThat(httpResponse).hasStatus(HttpStatus.OK)
+                .bodyJson()
+                .convertTo(InstanceOfAssertFactories.list(PostResponse.class))
+                .hasSize(2);
     }
 
     @Test
---
+++ src/test/resources/sql/tokens/add_test_token.sql
@@ -0,0 +1,1 @@
+INSERT INTO tokens (token, created_at, description) VALUES ('test-token-123', CURRENT_TIMESTAMP, 'Test token');
